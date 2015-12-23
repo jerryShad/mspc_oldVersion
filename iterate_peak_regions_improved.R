8a0ddd9f1febee9c17f5018c660cad13a2cf4793
polimi911 <juratbupt@gmail.com>
2015-12-23 12:13:09 +0100

    Create iterate_peak_regions_improved.R

---
+++ iterate_peak_regions_improved.R
@@ -0,0 +1,14 @@
+## iterate peak peak regions
+
+chrom= IRanges(peak$chromStart, peak$chormEnd)
+results<-IRanges::as.data.frame(overlap)
+
+ for (i in seq(len=length(chrom))) {
+      peak_start <- start(peak$chromStart)[i]
+      peak_end <- end(peak$chromEnd)[i]
+      peak_IR <- IRanges(peak_start, peak_End)
+      
+      # how can I scan through the peak region in target gene; how to iterate them
+      
+      overlap<-findOverlap(peak_IR, target_gene, algorithm=" intervaltree")
+    }
